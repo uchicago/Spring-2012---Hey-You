304b999e0966b96b2a9add09bd0d49d6ff5c08f6
T. Andrew Binkowski <abinkowski@uchicago.edu>
2012-05-10 10:26:11 -0500

    Initial Commit

---
+++ HeyYou/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HeyYou
+//
+//  Created by T. Binkowski on 5/10/12.
+//  Copyright (c) 2012 University of Chicago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HeyYou/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HeyYou
+//
+//  Created by T. Binkowski on 5/10/12.
+//  Copyright (c) 2012 University of Chicago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
